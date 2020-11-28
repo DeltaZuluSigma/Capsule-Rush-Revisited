9916513a4153725926d5d958f138fc8782c33937
DeltaZulu <bbgun567@gmail.com>
2020-11-27 21:39:56 -0500

    Add files via upload
    Main Menu, Power Up Duration HUD, Heart Count HUD, Coin Count HUD

---
+++ Assets/Scripts/DurationBar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//[ExecuteInEditMode()]
+public class DurationBar : MonoBehaviour
+{
+    public int max;
+    public int current;
+    public Image mask;
+    //private float elapsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        max = 100;
+        current = 0;
+        //elapsed = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(current > 0) { current -= 1; }
+        GetCurrentFill();
+    }
+
+    void GetCurrentFill()
+    {
+        float fillAmount = (float)current / (float)max;
+        mask.fillAmount = fillAmount;
+    }
+}
