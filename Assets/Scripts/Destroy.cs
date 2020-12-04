7d3b61574162adad51f8509088c05f2bcbfc5711
Kyle Cacilhas <71301402+KyleCacilhas@users.noreply.github.com>
2020-12-03 22:34:42 -0500

    Powerups
    Coin, +1 and powerups are included as prefabs. Each have a Rotate script and Destroy script.

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision col)
+    {
+        //Replace CapsuleBlue with whatever the tag name of the
+        //Player character is. Note: If the player has multiple tags
+        //due to powerups, then use || to distinguish each.
+        //E.g. col.gameObject.tag == "CapsuleBlue" || col.gameObject.tag == "CapsuleRed" || etc.
+        if (col.gameObject.tag == "CapsuleBlue")
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
