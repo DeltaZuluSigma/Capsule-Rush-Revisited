d31ebb86d5f1621c468b0e0a348fbcfb36fe6693
DeltaZulu <bbgun567@gmail.com>
2020-12-11 23:33:47 -0500

    Add files via upload
    - Added Splash / Landing Scene

---
+++ Scripts/AnyButtonToContinue.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnyButtonToContinue : MonoBehaviour
+{
+    public SceneChange sc;
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.anyKey)
+        {
+            sc.Change("MainMenu");
+        }
+    }
+}
